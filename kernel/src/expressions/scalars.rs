88fd54c90beee6c9e97835929ed9d8543e7a29ea
Robert Pack <robstar.pack@gmail.com>
2025-02-04 13:33:43 +0100

    chore: revert partialeq releated changes
    Signed-off-by: Robert Pack <robstar.pack@gmail.com>

--- kernel/src/expressions/scalars.rs
+++ kernel/src/expressions/scalars.rs
@@ -89,7 +89,7 @@ impl StructData {
 
 /// A single value, which can be null. Used for representing literal values
 /// in [Expressions][crate::expressions::Expression].
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Scalar {
     /// 32bit integer
     Integer(i32),
@@ -224,12 +224,6 @@ impl Display for Scalar {
     }
 }
 
-impl PartialEq for Scalar {
-    fn eq(&self, other: &Scalar) -> bool {
-        self.partial_cmp(other) == Some(Ordering::Equal)
-    }
-}
-
 impl PartialOrd for Scalar {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         use Scalar::*;
@@ -260,15 +254,10 @@ impl PartialOrd for Scalar {
             (Date(_), _) => None,
             (Binary(a), Binary(b)) => a.partial_cmp(b),
             (Binary(_), _) => None,
-            (Decimal(v1, _, s1), Decimal(v2, _, s2)) => {
-                let lhs = rust_decimal::Decimal::from_i128_with_scale(*v1, *s1 as u32);
-                let rhs = rust_decimal::Decimal::from_i128_with_scale(*v2, *s2 as u32);
-                lhs.partial_cmp(&rhs)
-            }
-            (Decimal(_, _, _), _) => None,
-            (Null(_), _) => None, // NOTE: NULL values are incomparable by definition
-            (Struct(_), _) => None, // TODO: Support Struct?
-            (Array(_), _) => None, // TODO: Support Array?
+            (Decimal(_, _, _), _) => None, // TODO: Support Decimal
+            (Null(_), _) => None,          // NOTE: NULL values are incomparable by definition
+            (Struct(_), _) => None,        // TODO: Support Struct?
+            (Array(_), _) => None,         // TODO: Support Array?
         }
     }
 }
@@ -596,7 +585,6 @@ mod tests {
         assert_eq!(&format!("{}", column_op), "3.1415927 IN Column(item)");
         assert_eq!(&format!("{}", column_not_op), "'Cool' NOT IN Column(item)");
     }
-
     #[test]
     fn test_timestamp_parse() {
         let assert_timestamp_eq = |scalar_string, micros| {
@@ -611,7 +599,6 @@ mod tests {
         assert_timestamp_eq("2011-01-11 13:06:07.123456", 1294751167123456);
         assert_timestamp_eq("1970-01-01 00:00:00", 0);
     }
-
     #[test]
     fn test_timestamp_ntz_parse() {
         let assert_timestamp_eq = |scalar_string, micros| {
@@ -639,37 +626,5 @@ mod tests {
 
         let p_type = PrimitiveType::Timestamp;
         assert_timestamp_fails(&p_type, "1971-07-22");
-    }
-
-    #[test]
-    fn test_partial_cmp() {
-        let a = Scalar::Integer(1);
-        let b = Scalar::Integer(2);
-        let c = Scalar::Null(DataType::INTEGER);
-        assert_eq!(a.partial_cmp(&b), Some(Ordering::Less));
-        assert_eq!(b.partial_cmp(&a), Some(Ordering::Greater));
-        assert_eq!(a.partial_cmp(&a), Some(Ordering::Equal));
-        assert_eq!(b.partial_cmp(&b), Some(Ordering::Equal));
-        assert_eq!(a.partial_cmp(&c), None);
-        assert_eq!(c.partial_cmp(&a), None);
-
-        // assert that NULL values are incomparable
-        let null = Scalar::Null(DataType::INTEGER);
-        assert_eq!(null.partial_cmp(&null), None);
-    }
-
-    #[test]
-    fn test_partial_eq() {
-        let a = Scalar::Integer(1);
-        let b = Scalar::Integer(2);
-        let c = Scalar::Null(DataType::INTEGER);
-        assert!(!a.eq(&b));
-        assert!(a.eq(&a));
-        assert!(!a.eq(&c));
-        assert!(!c.eq(&a));
-
-        // assert that NULL values are incomparable
-        let null = Scalar::Null(DataType::INTEGER);
-        assert!(!null.eq(&null));
     }
 }
